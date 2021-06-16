b7db02f800249ad60031560fdb650cbb1f9b93e9
wanderingrider <78330447+wanderingrider@users.noreply.github.com>
2021-06-16 18:56:49 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -38,3 +38,12 @@
 // console.log(val5);
 
 /** テンプレート文字列 */
+const name = "　小さな森";
+const age = 30;
+//「私の名前は小さな森です年齢は30歳です。」
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
