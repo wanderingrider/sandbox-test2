39b90860717c8a2e32ed7fd4bae76d3355c605e6
wanderingrider <78330447+wanderingrider@users.noreply.github.com>
2021-06-16 21:26:52 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -38,12 +38,36 @@
 // console.log(val5);
 
 /** テンプレート文字列 */
-const name = "　小さな森";
-const age = 30;
-//「私の名前は小さな森です年齢は30歳です。」
-// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-// console.log(message1);
+// const name = "　小さな森";
+// const age = 30;
+// //「私の名前は小さな森です年齢は30歳です。」
+// // const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// // console.log(message1);
 
-//テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/** アロー関数 */
+//　従来の関数
+// function func1(str){
+//   return str;
+// }
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 20));
