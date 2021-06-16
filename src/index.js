52ba7c7e9ae95014573f85f466596e8f8fd4013f
wanderingrider <78330447+wanderingrider@users.noreply.github.com>
2021-06-16 16:01:02 +0900

    const,var,let test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-var val1 = "var変数";
-console.log(val1);
+// // // var val1 = "var変数";
+// // // console.log(val1);
+
+// // // val1 = "var変数上書き";
+// // // console.log(val1);
+
+// // // var val1 = "var1を再宣言";
+// // // console.log(val1);
+
+// // let val2 = "let変数";
+// // console.log(val2);
+
+// // val2 = "let変数を上書き";
+// // console.log(val2);
+
+// // let val2 = "letを再宣言";
+
+// // const val3 = "const変数";
+// // console.log(val3);
+
+// // // val3 = "conset.変数を上書き";
+
+// // const val3 = "constを再宣言";
+
+// //constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "小さな森",
+//   age: 30
+// };
+// val4.name = "smallforest";
+// val4.address = "Shiga";
+// console.log(val4);
+
+// //constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/** テンプレート文字列 */
