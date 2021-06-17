f0379aa4a9ccdc68896043b6385c5f30e50214c7
wanderingrider <78330447+wanderingrider@users.noreply.github.com>
2021-06-17 11:57:47 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -182,3 +182,23 @@
 //   return num1 + num2 > 100 ? "100を超えています!" : "許容範囲内です";
 // };
 // console.log(checkSum(50, 40));
+
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || falg2) {
+//   console.log("1か2はtrueとなります");
+//
+// if ( flag1 && flag2) {
+//   console.log('1も2もtrueとなります');
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || '金額が未設定です';
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
